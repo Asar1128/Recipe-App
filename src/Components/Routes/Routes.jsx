8929a3e1843db647071c643b4a1d7d3224acb1e7
Asar1128 <coder221829@gmail.com>
2025-01-24 23:49:14 +0500

    Update

--- src/Components/Routes/Routes.jsx
+++ src/Components/Routes/Routes.jsx
@@ -1,31 +1,34 @@
-import { createBrowserRouter,  RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from '../../App/App';
 import AddRecipe from '../AddRecipe';
 import RecipeList from '../RecipeList';
+
 const router = createBrowserRouter(
     [
-   {
-    path:'/',
-    element:<App />                                                                                                                         
-   },
-   {
-    path:'/Add Recipe/:id',
-    element:<AddRecipe />
-   },
-   {
-    path:'/RecipeList',
-    element:<RecipeList/>
-   }
+        {
+            path: '/',
+            element: <App />
+        },
+        {
+            path: '/Add Recipe/:id',
+            element: <AddRecipe />
+        },
+        {
+            path: '/RecipeList',
+            element: <RecipeList />
+        }
+    ],
+    {
+        basename: '/Recipe_App' 
+    }
+);
 
-    ]
-)
-
-const RouterWrapper =  ()=>{
-    return(
+const RouterWrapper = () => {
+    return (
         <RouterProvider router={router}>
             <div>Loading...</div>
         </RouterProvider>
-    )
-}
+    );
+};
 
-export default RouterWrapper
+export default RouterWrapper;
